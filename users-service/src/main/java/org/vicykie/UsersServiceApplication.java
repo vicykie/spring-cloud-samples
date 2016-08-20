38099479e05a3bdadde8a2be92efdfeaa0be2e77
vicykie <122811575@qq.com>
2016-08-20 16:50:38 +0800

    Initial commit

---
+++ users-service/src/main/java/org/vicykie/UsersServiceApplication.java
@@ -0,0 +1,12 @@
+package org.vicykie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersServiceApplication.class, args);
+	}
+}
