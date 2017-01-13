b759794e52f50245ea652afc1da63d56bd0b00df
vicykie <122811575@qq.com>
2017-01-13 11:35:52 +0800

    1.配置中心添加基本认证； 2.所有从配置中心读取的配置需要增加基本认证的用户名和密码； 3.bootstrap.yml > application.yml

--- config-center/src/main/java/org/vicykie/ConfigCenterApplication.java
+++ config-center/src/main/java/org/vicykie/ConfigCenterApplication.java
@@ -2,15 +2,14 @@ package org.vicykie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
 //@EnableDiscoveryClient
-public class MicroServiceConfigApplication {
+public class ConfigCenterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroServiceConfigApplication.class, args);
+		SpringApplication.run(ConfigCenterApplication.class, args);
 	}
 }
--- registry-center/src/main/java/org/vicykie/RegistryCenterApplication.java
+++ registry-center/src/main/java/org/vicykie/RegistryCenterApplication.java
@@ -2,14 +2,13 @@ package org.vicykie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class RegisterServerApplication {
+public class RegistryCenterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RegisterServerApplication.class, args);
+		SpringApplication.run(RegistryCenterApplication.class, args);
 	}
 }
